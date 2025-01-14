fe9033f1aa02dd6a10b47fa7831d7eece954fc8b
marekvonrogall <marekvonrogall@gmx.ch>
2025-01-15 00:37:05 +0100

    Fix grid line and texture alignment issues.

--- src/ImageService/generator.py
+++ src/ImageService/generator.py
@@ -22,8 +22,9 @@ def generate_image():
         bg_color = (214, 190, 150)  # Light Beige
         line_color = (153, 135, 108)  # Dark Beige
         line_width = 3
-        outline_width = 4
-        padding = 4 # Team color padding between line and texture
+        outline_color = (153, 135, 108)  # Dark Beige
+        outline_width = 3
+        padding = 3 # Team color padding between line and texture
 
         # Create the base image
         image = Image.new("RGBA", (img_size, img_size), bg_color)
@@ -33,17 +34,17 @@ def generate_image():
         cell_size = (img_size - (line_width * (grid_size - 1)) - (outline_width * 2)) / grid_size
 
         # Grid lines
-        for i in range(grid_size + 1):
-            # Vertical lines
-            x = round(i * cell_size) + outline_width + (line_width * i-1)
+        for i in range(grid_size - 1):
+            # Vertical lines                                          v --> i+1 because it appears to draw to the left of coordinate
+            x = outline_width + ((i+1) * cell_size) + (line_width * i+1)
             draw.line([(x, 0), (x, img_size - 1)], fill=line_color, width=line_width)
 
             # Horizontal lines
-            y = round(i * cell_size) + outline_width + (line_width * i-1)
+            y = outline_width + ((i+1) * cell_size) + (line_width * i+1)
             draw.line([(0, y), (img_size - 1, y)], fill=line_color, width=line_width)
 
         # Outline
-        draw.rectangle([0, 0, img_size - 1, img_size - 1], outline=line_color, width=outline_width)
+        draw.rectangle([0, 0, img_size - 1, img_size - 1], outline=outline_color, width=outline_width)
         
         # Add images from textures
         for item in items:
@@ -68,16 +69,19 @@ def generate_image():
                 texture_image = texture_image.convert("RGBA")
 
             # Resize texture
-            texture_image = texture_image.resize((int(cell_size - 2*padding), int(cell_size - 2*padding)))
+            # texture_image = texture_image.resize((int(cell_size - 2*padding), int(cell_size - 2*padding)))
 
             # Calculate texture position on grid
-            x0 = round(column * cell_size) + outline_width + (line_width * column) + padding
-            y0 = round(row * cell_size) + outline_width + (line_width * row) + padding
-            x1 = x0 + texture_image.width
-            y1 = y0 + texture_image.height
-            #x1 = x0 + (cell_size - 2*padding) # Might stretch texture, but ensures good styling
-            #y1 = y0 + (cell_size - 2*padding) # Might stretch texture, but ensures good styling
+            x0 = int(column * cell_size + outline_width + (line_width * column) + padding)
+            y0 = int(row * cell_size + outline_width + (line_width * row) + padding)
+            #x1 = x0 + texture_image.width
+            #y1 = y0 + texture_image.height
 
+            # Might stretch texture, but ensures good styling
+            x1 = x0 + int(cell_size - 2*padding)
+            y1 = y0 + int(cell_size - 2*padding)
+            texture_image = texture_image.resize((x1 - x0, y1 - y0))
+            
             # Paste texture on map image
             image.paste(texture_image, (x0, y0), texture_image)
 
@@ -87,7 +91,7 @@ def generate_image():
         image.save(filepath)
 
         # Return URL
-        return jsonify({"url": f"/public/{filename}"}), 201
+        return jsonify({"url": f"/public/{filename}", "cell_size": cell_size}), 201
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
