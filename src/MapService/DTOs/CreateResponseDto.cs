bfffde37de53a61e98c5f15189b62ae1f2ebe74c
Marek von Rogall <marekvonrogall@gmx.ch>
2025-12-27 04:24:52 +0100

    Add Json Property Names to CreateResponseDto.cs

--- src/MapService/DTOs/CreateResponseDto.cs
+++ src/MapService/DTOs/CreateResponseDto.cs
@@ -1,8 +1,13 @@
+using System.Text.Json.Serialization;
+
 namespace MapService.DTOs
 {
     public class CreateResponseDto
     {
+        [JsonPropertyName("map_url")]
         public required string MapUrl { get; set; }
+        
+        [JsonPropertyName("map_raw")]
         public required MapRawDto MapRaw { get; set; }
     }
 }
